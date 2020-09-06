0d36e937c2c60cefd9576e25c7f26339f9ddcddf
Brine <brian@qualityshepherd.com>
2020-09-06 11:12:28 -0600

    add delete test and refactor a bit to make use of axios defaults and verbage

--- tests/apiTest.js
+++ tests/apiTest.js
@@ -1,9 +1,13 @@
 import api from '../lib/apiModule'
 
-let body = {
+let data = {
   "userId": 1,
   "title": "I Like Eggs!",
   "body": "blah, blah, blah..."
+}
+
+let config = {
+  headers: {}
 }
 
 describe('GET /posts', () => {
@@ -12,6 +16,13 @@ describe('GET /posts', () => {
 
     expect(res.status).toBe(200)
     expect(res.data.length).toBeGreaterThan(1)
+  })
+
+  test('should get a single post', async () => {
+    const res = await api.get('/posts/42')
+
+    expect(res.status).toBe(200)
+    expect(res.data.id).toBe(42)
   })
 
   test('should error with nonexistent id', async () => {
@@ -23,21 +34,29 @@ describe('GET /posts', () => {
 
 describe('POST /posts', () => {
   test('should create a new post', async () => {
-    const response = await api.post('/posts', body)
+    const response = await api.post('/posts', data)
 
     expect(response.status).toBe(201)
     expect(response.data.id).toBeDefined() // the app does not update but this should exist
-    expect(response.data.body).toBe(body.body)
+    expect(response.data.body).toBe(data.body)
   })
 })
 
 describe('PUT /posts/{id}', () => {
   test('should update a post', async () => {
-    body.body = "body was updated!"
-    const response = await api.put('/posts/42', body)
+    data.body = "body was updated!"
+    const response = await api.put('/posts/42', data)
 
     expect(response.status).toBe(200)
-    expect(response.data.body).toBe(body.body)
+    expect(response.data.body).toBe(data.body)
     expect(response.data.id).toBe(42)
+  })
+})
+
+describe('DELETE /posts/{id}', () => {
+  test('should delete a post', async () => {
+    const response = await api.delete('/posts/999999999', data)
+
+    expect(response.status).toBe(200)
   })
 })
